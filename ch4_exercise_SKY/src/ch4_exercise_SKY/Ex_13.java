400e7cbf4e838c78c91c3f3277f18e3c6c8b7886
haneul <msky0104@naver.com>
2021-07-26 00:00:07 +0900

    4-13

---
+++ ch4_exercise_SKY/src/ch4_exercise_SKY/Ex_13.java
@@ -0,0 +1,27 @@
+package ch4_exercise_SKY;
+
+public class Ex_13 {
+
+	public static void main(String[] args) {
+		
+		String value = "12o34";
+		char ch = ' ';
+		boolean isNumber = true;
+		
+		// 반복문과 charAt(int i)를 이용해서 문자열의 문자를
+		// 하나씩 읽어서 검사한다.
+		for(int i=0; i < value.length() ;i++) {
+			int tmp= value.charAt(i)-'0';
+			if(tmp<=0 || tmp >=9) {
+				isNumber = false;
+				break;
+			}
+		}
+		
+		if (isNumber) {
+			System.out.println(value+"는 숫자입니다.");
+		} else {
+			System.out.println(value+"는 숫자가 아닙니다.");
+		}
+	}
+}
