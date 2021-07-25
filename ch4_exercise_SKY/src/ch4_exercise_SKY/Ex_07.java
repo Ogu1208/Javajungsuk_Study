f70b2f775e92ab4076f322791b76a507874abc3f
haneul <msky0104@naver.com>
2021-07-25 22:40:57 +0900

    4-7

---
+++ ch4_exercise_SKY/src/ch4_exercise_SKY/Ex_07.java
@@ -0,0 +1,10 @@
+package ch4_exercise_SKY;
+
+public class Ex_07 {
+
+	public static void main(String[] args) {
+		int value = ((int)(Math.random()*6)+1);
+		System.out.println("value:"+value);
+	}
+
+}
