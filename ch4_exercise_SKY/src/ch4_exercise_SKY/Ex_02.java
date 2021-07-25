8a3299df7aba424b8e3b0fb3b0166fdc82e2a23d
haneul <msky0104@naver.com>
2021-07-25 20:51:25 +0900

    4-2

---
+++ ch4_exercise_SKY/src/ch4_exercise_SKY/Ex_02.java
@@ -0,0 +1,13 @@
+package ch4_exercise_SKY;
+
+public class Ex_02 {
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=1; i<=20; i++) {
+			if(i%2!=0 && i%3!=0) {
+				sum+=i;
+			}
+		}
+		System.out.println(sum);
+	}
+}
