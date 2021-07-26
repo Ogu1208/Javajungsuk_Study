9a6cd3c1d33be0c7a8baf0ce6b69466aa0975b3f
haneul <msky0104@naver.com>
2021-07-26 23:05:29 +0900

    testttt

--- ch5_exercise_SKY/src/ch5_exercise_SKY/Ex_test.java
+++ ch5_exercise_SKY/src/ch5_exercise_SKY/Ex_test.java
@@ -3,7 +3,7 @@ package ch5_exercise_SKY;
 public class Ex_test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub test
 
 	}
 
