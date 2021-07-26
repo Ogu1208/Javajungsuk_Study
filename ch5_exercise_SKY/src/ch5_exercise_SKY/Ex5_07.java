08238d01b38fdc2f68595db12adcde5dd68135ad
haneul <msky0104@naver.com>
2021-07-26 18:40:49 +0900

    5-7

--- ch5_exercise_SKY/src/ch5_exercise_SKY/Ex5_07.java
+++ ch5_exercise_SKY/src/ch5_exercise_SKY/Ex5_07.java
@@ -3,8 +3,50 @@ package ch5_exercise_SKY;
 public class Ex5_07 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		if(args.length!=1) {
+			System.out.println("USAGE: java Exercise5_7 3120");
+			System.exit(0);
+		}
+		
+			// 문자열을 숫자로 변환한다. 입력한 값이 숫자가 아닐 경우 예외가 발생한다.
+		int money = Integer.parseInt(args[0]);
+		
+		System.out.println("money="+money);
+			
+		int[] coinUnit = {500, 100, 50, 10 }; // 동전의 단위
+		int[] coin = {5, 5, 5, 5}; // 단위별 동전의 개수
+			
+		for(int i=0;i<coinUnit.length;i++) {
+				int coinNum = 0;
+				coinNum= money/coinUnit[i];
+				if(coin[i] >= coinNum) {
+					coin[i] -= coinNum;
+					money=money-coinNum*coinUnit[i];
+				}
+				else {
+					coinNum= coin[i];
+					coin[i]=0;
+					money=money-coinNum*coinUnit[i];
+				}
+				
+			/* (1) 아래의 로직에 맞게 코드를 작성하시오.
+			1. 금액(money)을 동전단위로 나눠서 필요한 동전의 개수(coinNum)를 구한다.
+			2. 배열 coin에서 coinNum만큼의 동전을 뺀다.
+			(만일 충분한 동전이 없다면 배열 coin에 있는 만큼만 뺀다.)
+			3. 금액에서 동전의 개수(coinNum)와 동전단위를 곱한 값을 뺀다.
+			*/
+			System.out.println(coinUnit[i]+"원: "+coinNum);
+		}
+		
+		if(money > 0) {
+			System.out.println("거스름돈이 부족합니다.");
+			System.exit(0); // 프로그램을 종료한다.
+		}
+			System.out.println("=남은 동전의 개수 =");
+			
+		for(int i=0;i<coinUnit.length;i++) {
+			System.out.println(coinUnit[i]+"원:"+coin[i]);
+		}
 	}
 
 }
