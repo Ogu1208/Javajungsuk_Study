0051db51747e73556df50729aa526930a5ad236d
Ogu1208 <76902448+Ogu1208@users.noreply.github.com>
2021-07-26 23:45:08 +0900

    s

--- ch4_exercise_Ogu/src/ch4_exercise_Ogu/Exercise4_2.java
+++ ch4_exercise_Ogu/src/ch4_exercise_Ogu/Exercise4_2.java
@@ -1,7 +1,7 @@
 package ch4_exercise_Ogu;
 
 public class Exercise4_2 {
-		// edit
+		// edit222222222
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i, sum = 0;
