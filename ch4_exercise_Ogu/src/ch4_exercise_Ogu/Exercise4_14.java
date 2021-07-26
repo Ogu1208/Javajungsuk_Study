f64e71b27860058289b738611370c388f3cd63bf
Ogu1208 <76902448+Ogu1208@users.noreply.github.com>
2021-07-26 23:46:51 +0900

    Hi

--- ch4_exercise_Ogu/src/ch4_exercise_Ogu/Exercise4_14.java
+++ ch4_exercise_Ogu/src/ch4_exercise_Ogu/Exercise4_14.java
@@ -4,21 +4,7 @@ public class Exercise4_14 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 Scanner scanner = new Scanner(System.in);
-	        int computerNumber = (int)(Math.random() * 100) + 1;
-	        int userNumber = 0;
-	        int trial = 0;
-	        while(userNumber != computerNumber){
-	            System.out.println("1과 100사이의 값을 입력하세요.");
-	            userNumber = scanner.nextInt();
-	            if(userNumber > computerNumber){
-	                System.out.println("더 작은 수를 입력하세요.");
-	            }else{
-	                System.out.println("더 큰수를 입력하세요.");
-	            }
-	            trial++;
-	        }
-	        System.out.println("맞췄습니다.\n시도횟수는 " + trial + "번입니다.");
+		 System.out.println("Hi");
 	}
 
 }
