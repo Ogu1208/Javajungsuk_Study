c12be23bc64c7f51d9c0eeaa0137183d7e32e349
haneul <msky0104@naver.com>
2021-08-02 21:39:52 +0900

    ++ 7-22 7-23

--- ch7_exercise_SKY/src/Ex7_22/Ex7_22.java
+++ ch7_exercise_SKY/src/Ex7_22/Ex7_22.java
@@ -4,6 +4,23 @@ public class Ex7_22 {
 	public static void main(String args[]) {}
 }
 
+	class Point {
+		int x;
+		int y;
+		Point() {
+			this(0,0);	
+		}
+	
+		Point(int x, int y) {
+			this.x=x;
+			this.y=y;
+		}
+		
+		public String toString() {
+			return "["+x+","+y+"]";
+		}
+	}
+
 	abstract class Shape {
 		Point p;
 		Shape() {
@@ -23,26 +40,25 @@ public class Ex7_22 {
 		}
 	}
 
-	class Point {
-		int x;
-		int y;
-		Point() {
-			this(0,0);	
-		}
-		
-		Point(int x, int y) {
-			this.x=x;
-			this.y=y;
-		}
-		
-		public String toString() {
-			return "["+x+","+y+"]";
-		}
-	}
+	
 
 	class Circle extends Shape {
 		double r;
 		final double PI = 3.14;
+		
+		
+		Circle(){
+			super(new Point(0,0));
+			this.r=1;
+		}
+		Circle(double r){
+			super(new Point(0,0));
+			this.r=r;
+		}
+		Circle(Point p, Double r){
+			super.p=p;
+			this.r=r;
+		}
 		
 		double calcArea() {
 			return r*r*PI;
@@ -55,8 +71,19 @@ public class Ex7_22 {
 		double height;
 		
 		Rectangle(){
+			super(new Point(0,0));
 			this.width=1;
 			this.height=1;
+		}
+		Rectangle(double width,double height){
+			super(new Point(0,0));
+			this.width=width;
+			this.height=height;
+		}
+		Rectangle(Point p, double width,double height){
+			super.p=p;
+			this.width=width;
+			this.height=height;
 		}
 		
 		double calcArea() {
