86021c7ef1e0d22ff0dae5967b27b0d8f5eb6058
Ogu1208 <76902448+Ogu1208@users.noreply.github.com>
2021-08-14 21:45:07 +0900

    Ch8 Exercise commit _Ogu

---
+++ ch8_exercise_Ogu/src/ch8_exercise_Ogu/Ex8_9.java
@@ -0,0 +1,31 @@
+package ch8_exercise_Ogu;
+
+class UnsupportedFuctionException extends RuntimeException{
+	// 에러 코드 값을 저장하기 위한 필드를 추가
+	private final int ERR_CODE;
+	UnsupportedFuctionException(String msg, int errCode) {
+		super(msg);
+		ERR_CODE = errCode;
+	}
+	
+	UnsupportedFuctionException(String msg) { // 생성자
+		this(msg, 100);
+	}
+	
+	public int getErrCode() {
+		return ERR_CODE;
+	}
+	
+	public String getMessage() {
+		return "[" + getErrCode() + "]" + super.getMessage();
+	}
+}
+
+public class Ex8_9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedFuctionException("지원하지 않는 기능입니다.",100); 
+	}
+
+}
