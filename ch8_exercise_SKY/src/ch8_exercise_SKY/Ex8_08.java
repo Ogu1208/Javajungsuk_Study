be8173eebb883313cfc809428dc956a95aafe79e
haneul <msky0104@naver.com>
2021-08-14 22:20:54 +0900

    ch8

---
+++ ch8_exercise_SKY/src/ch8_exercise_SKY/Ex8_08.java
@@ -0,0 +1,32 @@
+package ch8_exercise_SKY;
+
+import java.util.*;
+public class Ex8_08 {
+	public static void main(String[] args) {
+		// 1~100사이의 임의의 값을 얻어서 answer에 저장한다.
+		int answer = (int)(Math.random() * 100) + 1;
+		int input = 0; // 사용자입력을 저장할 공간
+		int count = 0; // 시도횟수를 세기 위한 변수
+		do {
+			try {
+				count++;
+				System.out.print("1과 100사이의 값을 입력하세요 :");
+				input = new Scanner(System.in).nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("유효하지 않은 값입니다. 다시 값을 입력해주세요.");
+				count--;
+				continue;
+			}
+			if(answer > input) {
+				System.out.println("더 큰 수를 입력하세요.");
+			} else if(answer < input) {
+				System.out.println("더 작은 수를 입력하세요.");
+			} else {
+				System.out.println("맞췄습니다.");
+				System.out.println("시도횟수는 "+count+"번입니다.");
+				break; // do-while문을 벗어난다
+			}
+		} while(true); // 무한반복문
+		
+	} // end of main
+} 
---
+++ ch8_exercise_SKY/src/ch8_exercise_SKY/Ex8_09.java
@@ -0,0 +1,23 @@
+package ch8_exercise_SKY;
+
+public class Ex8_09 {
+
+	class UnsupportedFuctionException extends RuntimeException{
+		final private int ERR_CODE;
+		
+		public int getErrorCode() {
+			return ERR_CODE;
+		}
+		
+		public String getMessage() {
+			return getMessage();
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		throw new UnsupportedFuctionException("지원하지 않는 기능입니다.",100);
+	}
+}
+
+// 생성자 만들기 포기
