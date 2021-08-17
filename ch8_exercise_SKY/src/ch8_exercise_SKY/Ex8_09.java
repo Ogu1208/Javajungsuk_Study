bb233a1926789ec82a221fd55212e00af01a210e
haneul <msky0104@naver.com>
2021-08-18 05:53:21 +0900

    2

--- ch8_exercise_SKY/src/ch8_exercise_SKY/Ex8_09.java
+++ ch8_exercise_SKY/src/ch8_exercise_SKY/Ex8_09.java
@@ -5,14 +5,14 @@ public class Ex8_09 {
 	class UnsupportedFuctionException extends RuntimeException{
 	//	final private int ERR_CODE;
 		
-		public int getErrorCode() {
-			return ERR_CODE;
+		//public int getErrorCode() {
+		//	return ERR_CODE;
 		}
 		
 		public String getMessage() {
 			return getMessage();
 		}
-	}
+	//}
 	
 	
 	public static void main(String[] args) {
